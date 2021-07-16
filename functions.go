06288684285d808298f4ede8389fbd5f7451f38e
Mario Malcek <mmalcek@gmail.com>
2021-07-16 17:26:51 +0200

    code refactoring + tests

--- functions.go
+++ functions.go
@@ -27,30 +27,18 @@ import (
 // templateFunctions extends template functions
 func templateFunctions() template.FuncMap {
 	return template.FuncMap{
-		"lua":     luaF,
-		"add":     add,
-		"add1":    func(i interface{}) int64 { return toInt64(i) + 1 },
-		"sub":     func(a, b interface{}) int64 { return toInt64(a) - toInt64(b) },
-		"div":     func(a, b interface{}) int64 { return toInt64(a) / toInt64(b) },
-		"mod":     func(a, b interface{}) int64 { return toInt64(a) % toInt64(b) },
-		"mul":     mul,
-		"randInt": func(min, max int) int { return rand.Intn(max-min) + min },
-		"add1f": func(i interface{}) float64 {
-			return execDecimalOp(i, []interface{}{1}, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Add(d2) })
-		},
-		"addf": func(i ...interface{}) float64 {
-			a := interface{}(float64(0))
-			return execDecimalOp(a, i, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Add(d2) })
-		},
-		"subf": func(a interface{}, v ...interface{}) float64 {
-			return execDecimalOp(a, v, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Sub(d2) })
-		},
-		"divf": func(a interface{}, v ...interface{}) float64 {
-			return execDecimalOp(a, v, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Div(d2) })
-		},
-		"mulf": func(a interface{}, v ...interface{}) float64 {
-			return execDecimalOp(a, v, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Mul(d2) })
-		},
+		"add":        add,
+		"add1":       add1,
+		"sub":        sub,
+		"div":        div,
+		"mod":        mod,
+		"mul":        mul,
+		"addf":       addf,
+		"add1f":      add1f,
+		"subf":       subf,
+		"divf":       divf,
+		"mulf":       mulf,
+		"randInt":    randInt,
 		"round":      round,
 		"max":        max,
 		"min":        min,
@@ -62,15 +50,15 @@ func templateFunctions() template.FuncMap {
 		"b64dec":     base64decode,
 		"b32enc":     base32encode,
 		"b32dec":     base32decode,
-		"uuid":       func() string { return uuid.New().String() },
+		"uuid":       newUUID,
 		"regexMatch": regexMatch,
-		"upper":      strings.ToUpper,
-		"lower":      strings.ToLower,
-		"trim":       strings.TrimSpace,
-		"trimAll":    func(a, b string) string { return strings.Trim(a, b) },
-		"trimSuffix": func(a, b string) string { return strings.TrimSuffix(a, b) },
-		"trimPrefix": func(a, b string) string { return strings.TrimPrefix(a, b) },
-		"atoi":       func(a string) int { i, _ := strconv.Atoi(a); return i },
+		"upper":      upper,
+		"lower":      lower,
+		"trim":       trim,
+		"trimAll":    trimAll,
+		"trimSuffix": trimSuffix,
+		"trimPrefix": trimPrefix,
+		"atoi":       atoi,
 		"int":        toInt,
 		"int64":      toInt64,
 		"float64":    toFloat64,
@@ -79,16 +67,224 @@ func templateFunctions() template.FuncMap {
 		"toYAML":     toYAML,
 		"toXML":      toXML,
 		"mapJSON":    mapJSON,
+		"lua":        luaF,
 	}
 }
 
-// mapJSON string JSON to map[string]interface{} so it can be used in pipline -> template
-func mapJSON(input string) map[string]interface{} {
-	var mapData map[string]interface{}
-	if err := json.Unmarshal([]byte(input), &mapData); err != nil {
-		log.Fatalf("errorMapJSON: %s\n", err.Error())
+// add count
+func add(i ...interface{}) int64 {
+	var a int64 = 0
+	for _, b := range i {
+		a += toInt64(b)
 	}
-	return mapData
+	return a
+}
+
+// add1 input+1
+func add1(i interface{}) int64 { return toInt64(i) + 1 }
+
+// sub substitute
+func sub(a, b interface{}) int64 { return toInt64(a) - toInt64(b) }
+
+// div divide
+func div(a, b interface{}) int64 { return toInt64(a) / toInt64(b) }
+
+// mod modulo
+func mod(a, b interface{}) int64 { return toInt64(a) % toInt64(b) }
+
+// mul multiply
+func mul(a interface{}, v ...interface{}) int64 {
+	val := toInt64(a)
+	for _, b := range v {
+		val = val * toInt64(b)
+	}
+	return val
+}
+
+// addf count float
+func addf(i ...interface{}) float64 {
+	a := interface{}(float64(0))
+	return execDecimalOp(a, i, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Add(d2) })
+}
+
+// add1f inputFloat+1
+func add1f(i interface{}) float64 {
+	return execDecimalOp(i, []interface{}{1}, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Add(d2) })
+}
+
+// subf substitute float
+func subf(a interface{}, v ...interface{}) float64 {
+	return execDecimalOp(a, v, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Sub(d2) })
+}
+
+// divide float
+func divf(a interface{}, v ...interface{}) float64 {
+	return execDecimalOp(a, v, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Div(d2) })
+}
+
+// mulf multiply float
+func mulf(a interface{}, v ...interface{}) float64 {
+	return execDecimalOp(a, v, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Mul(d2) })
+}
+
+// randInt returns random integer in defined range {{randInt min max}} e.g. {{randInt 1 10}}
+func randInt(min, max int) int { return rand.Intn(max-min) + min }
+
+// round float {{round .val 2}} -> 2 decimals
+func round(a interface{}, p int, rOpt ...float64) float64 {
+	roundOn := .5
+	if len(rOpt) > 0 {
+		roundOn = rOpt[0]
+	}
+	val := toFloat64(a)
+	places := toFloat64(p)
+
+	var round float64
+	pow := math.Pow(10, places)
+	digit := pow * val
+	_, div := math.Modf(digit)
+	if div >= roundOn {
+		round = math.Ceil(digit)
+	} else {
+		round = math.Floor(digit)
+	}
+	return round / pow
+}
+
+// max return highest from numbers {{max .v1 .v2 .v3}}
+func max(a interface{}, i ...interface{}) int64 {
+	aa := toInt64(a)
+	for _, b := range i {
+		bb := toInt64(b)
+		if bb > aa {
+			aa = bb
+		}
+	}
+	return aa
+}
+
+// min return lowest from numbers {{min .v1 .v2 .v3}}
+func min(a interface{}, i ...interface{}) int64 {
+	aa := toInt64(a)
+	for _, b := range i {
+		bb := toInt64(b)
+		if bb < aa {
+			aa = bb
+		}
+	}
+	return aa
+}
+
+// maxf return highest from float numbers {{maxf .v1 .v2 .v3}}
+func maxf(a interface{}, i ...interface{}) float64 {
+	aa := toFloat64(a)
+	for _, b := range i {
+		bb := toFloat64(b)
+		aa = math.Max(aa, bb)
+	}
+	return aa
+}
+
+// minf return lowest from float numbers {{minf .v1 .v2 .v3}}
+func minf(a interface{}, i ...interface{}) float64 {
+	aa := toFloat64(a)
+	for _, b := range i {
+		bb := toFloat64(b)
+		aa = math.Min(aa, bb)
+	}
+	return aa
+}
+
+// dateFormat convert date format {{dateFormat "string", "inputPattern", "outputPattern"}} e.g. {{dateFormat "15.03.2021", "02.01.2006", "01022006"}}
+func dateFormat(date string, inputFormat string, outputFormat string) string {
+	timeParsed, err := time.Parse(inputFormat, date)
+	if err != nil {
+		return date
+	}
+	return timeParsed.Format(outputFormat)
+}
+
+// now return current date/time in specified format
+func now(format string) string {
+	return time.Now().Format(format)
+}
+
+// base64encode encode to base64
+func base64encode(v string) string {
+	return base64.StdEncoding.EncodeToString([]byte(v))
+}
+
+// base64decode decode from base64
+func base64decode(v string) string {
+	data, err := base64.StdEncoding.DecodeString(v)
+	if err != nil {
+		return err.Error()
+	}
+	return string(data)
+}
+
+// base32encode encode to base32
+func base32encode(v string) string {
+	return base32.StdEncoding.EncodeToString([]byte(v))
+}
+
+// base32decode decode from base32
+func base32decode(v string) string {
+	data, err := base32.StdEncoding.DecodeString(v)
+	if err != nil {
+		return err.Error()
+	}
+	return string(data)
+}
+
+// newUUID returns UUID
+func newUUID() string { return uuid.New().String() }
+
+// regexMatch check regex e.g. {{regexMatch "a.b", "aaxbb"}}
+func regexMatch(regex string, s string) bool {
+	match, _ := regexp.MatchString(regex, s)
+	return match
+}
+
+// upper string to uppercase
+func upper(s string) string {
+	return strings.ToUpper(s)
+}
+
+// lower string to lowercase
+func lower(s string) string {
+	return strings.ToLower(s)
+}
+
+func trim(s string) string {
+	return strings.TrimSpace(s)
+}
+
+// trimAll remove leading and trailing whitespace
+func trimAll(a, b string) string { return strings.Trim(a, b) }
+
+// {{trimPrefix "!Hello World!" "!"}} - returns "Hello World!"
+func trimPrefix(a, b string) string { return strings.TrimPrefix(a, b) }
+
+// trimSuffix - {{trimSuffix "!Hello World!" "!"}} - returns "!HelloWorld"
+func trimSuffix(a, b string) string { return strings.TrimSuffix(a, b) }
+
+// - atoi {{atoi "42"}} - string to int
+func atoi(a string) int { i, _ := strconv.Atoi(a); return i }
+
+// toInt convert to int
+func toInt(v interface{}) int {
+	return cast.ToInt(v)
+}
+
+// toInt64 converts integer types to 64-bit integers
+func toInt64(v interface{}) int64 {
+	return cast.ToInt64(v)
+}
+
+// toFloat64 converts 64-bit floats
+func toFloat64(v interface{}) float64 {
+	return cast.ToFloat64(v)
 }
 
 // toJSON convert to JSON
@@ -127,6 +323,15 @@ func toXML(data map[string]interface{}) string {
 	return string(out)
 }
 
+// mapJSON string JSON to map[string]interface{} so it can be used in pipline -> template
+func mapJSON(input string) map[string]interface{} {
+	var mapData map[string]interface{}
+	if err := json.Unmarshal([]byte(input), &mapData); err != nil {
+		log.Fatalf("errorMapJSON: %s\n", err.Error())
+	}
+	return mapData
+}
+
 // luaF Call LUA function {{lua "functionName" input1 input2 input3 ...}
 // 1. Functions must be placed in ./lua/functions, 2. Inputs are passed as stringified json 3. Output of lua function must be string
 func luaF(i ...interface{}) string {
@@ -146,152 +351,6 @@ func luaF(i ...interface{}) string {
 		return str.String()
 	}
 	return "luaError: getResult"
-}
-
-// dateFormat convert date format {{dateFormat "string", "inputPattern", "outputPattern"}} e.g. {{dateFormat "15.03.2021", "02.01.2006", "01022006"}}
-func dateFormat(date string, inputFormat string, outputFormat string) string {
-	timeParsed, err := time.Parse(inputFormat, date)
-	if err != nil {
-		return date
-	}
-	return timeParsed.Format(outputFormat)
-}
-
-// now return current date/time in specified format
-func now(format string) string {
-	return time.Now().Format(format)
-}
-
-// add count
-func add(i ...interface{}) int64 {
-	var a int64 = 0
-	for _, b := range i {
-		a += toInt64(b)
-	}
-	return a
-}
-
-// mul multiply
-func mul(a interface{}, v ...interface{}) int64 {
-	val := toInt64(a)
-	for _, b := range v {
-		val = val * toInt64(b)
-	}
-	return val
-}
-
-// max return highest from numbers {{max .v1 .v2 .v3}}
-func max(a interface{}, i ...interface{}) int64 {
-	aa := toInt64(a)
-	for _, b := range i {
-		bb := toInt64(b)
-		if bb > aa {
-			aa = bb
-		}
-	}
-	return aa
-}
-
-// maxf return highest from float numbers {{maxf .v1 .v2 .v3}}
-func maxf(a interface{}, i ...interface{}) float64 {
-	aa := toFloat64(a)
-	for _, b := range i {
-		bb := toFloat64(b)
-		aa = math.Max(aa, bb)
-	}
-	return aa
-}
-
-// min return lowest from numbers {{min .v1 .v2 .v3}}
-func min(a interface{}, i ...interface{}) int64 {
-	aa := toInt64(a)
-	for _, b := range i {
-		bb := toInt64(b)
-		if bb < aa {
-			aa = bb
-		}
-	}
-	return aa
-}
-
-// minf return lowest from float numbers {{minf .v1 .v2 .v3}}
-func minf(a interface{}, i ...interface{}) float64 {
-	aa := toFloat64(a)
-	for _, b := range i {
-		bb := toFloat64(b)
-		aa = math.Min(aa, bb)
-	}
-	return aa
-}
-
-// round float {{round .val 2}} -> 2 decimals
-func round(a interface{}, p int, rOpt ...float64) float64 {
-	roundOn := .5
-	if len(rOpt) > 0 {
-		roundOn = rOpt[0]
-	}
-	val := toFloat64(a)
-	places := toFloat64(p)
-
-	var round float64
-	pow := math.Pow(10, places)
-	digit := pow * val
-	_, div := math.Modf(digit)
-	if div >= roundOn {
-		round = math.Ceil(digit)
-	} else {
-		round = math.Floor(digit)
-	}
-	return round / pow
-}
-
-// base64encode encode to base64
-func base64encode(v string) string {
-	return base64.StdEncoding.EncodeToString([]byte(v))
-}
-
-// base64decode decode from base64
-func base64decode(v string) string {
-	data, err := base64.StdEncoding.DecodeString(v)
-	if err != nil {
-		return err.Error()
-	}
-	return string(data)
-}
-
-// base32encode encode to base32
-func base32encode(v string) string {
-	return base32.StdEncoding.EncodeToString([]byte(v))
-}
-
-// base32decode decode from base32
-func base32decode(v string) string {
-	data, err := base32.StdEncoding.DecodeString(v)
-	if err != nil {
-		return err.Error()
-	}
-	return string(data)
-}
-
-// regexMatch check regex e.g. {{regexMatch "a.b", "aaxbb"}}
-func regexMatch(regex string, s string) bool {
-	match, _ := regexp.MatchString(regex, s)
-	return match
-}
-
-// toFloat64 converts 64-bit floats
-func toFloat64(v interface{}) float64 {
-	return cast.ToFloat64(v)
-}
-
-// toInt convert to int
-func toInt(v interface{}) int {
-	return cast.ToInt(v)
-}
-
-// toInt64 converts integer types to 64-bit integers
-func toInt64(v interface{}) int64 {
-	return cast.ToInt64(v)
 }
 
 // execDecimalOp convert float to decimal
--- functions_test.go
+++ functions_test.go
@@ -1,124 +1,263 @@
 package main
 
 import (
+	"regexp"
 	"testing"
+	"time"
 
 	"github.com/shopspring/decimal"
 )
 
-func TestLuaF(t *testing.T) {
-	if luaF("sum", "5", "5") != "10" {
-		t.Errorf("luaF unexpected result: %s", luaF("sum", "5", "5"))
-	}
-}
-
-func TestDateFormat(t *testing.T) {
-	if dateFormat("15.03.2021", "02.01.2006", "01022006") != "03152021" {
-		t.Errorf("dateFormat unexpected result: %s", dateFormat("15.03.2021", "02.01.2006", "01022006"))
-	}
-}
-
 func TestAdd(t *testing.T) {
-	if add("6", 4) != 10 {
-		t.Errorf("add unexpected result: %v", add("6", 4))
+	result := add("6", 4)
+	if result != 10 {
+		t.Errorf("result: %v", result)
+	}
+}
+func TestAdd1(t *testing.T) {
+	result := add1("6")
+	if result != 7 {
+		t.Errorf("result: %v", result)
 	}
 }
 
+func TestSub(t *testing.T) {
+	result := sub("6", 2)
+	if result != 4 {
+		t.Errorf("result: %v", result)
+	}
+}
+
+func TestDiv(t *testing.T) {
+	result := div("6", 2)
+	if result != 3 {
+		t.Errorf("result: %v", result)
+	}
+}
+
+func TestMod(t *testing.T) {
+	result := mod("6", 5)
+	if result != 1 {
+		t.Errorf("result: %v", result)
+	}
+}
 func TestMul(t *testing.T) {
-	if mul("6", 4) != 24 {
-		t.Errorf("mul unexpected result: %v", mul("6", 4))
+	result := mul("6", 4)
+	if result != 24 {
+		t.Errorf("result: %v", result)
 	}
 }
 
-func TestMax(t *testing.T) {
-	if max("6", 4, "12", "5") != 12 {
-		t.Errorf("max unexpected result: %v", max("6", 4, "12", "5"))
+func TestAddf(t *testing.T) {
+	result := addf("6.14", 4.12)
+	if result != 10.26 {
+		t.Errorf("result: %v", result)
 	}
 }
 
-func TestMaxf(t *testing.T) {
-	if maxf("6.32", 4.15, "12.3128", "5") != 12.3128 {
-		t.Errorf("maxf unexpected result: %v", maxf("6.32", 4.15, "12.3128", "5"))
+func TestAdd1f(t *testing.T) {
+	result := add1f("6.14")
+	if result != 7.14 {
+		t.Errorf("result: %v", result)
 	}
 }
 
-func TestMin(t *testing.T) {
-	if min("6", 4, "12", "5") != 4 {
-		t.Errorf("min unexpected result: %v", min("6", 4, "12", "5"))
+func TestSubf(t *testing.T) {
+	result := subf("6.12487", 2.347511)
+	if result != 3.777359 {
+		t.Errorf("result: %v", result)
 	}
 }
 
-func TestMinf(t *testing.T) {
-	if minf("6.32", 4.15, "12.3128", "5") != 4.15 {
-		t.Errorf("minf unexpected result: %v", minf("6.32", 4.15, "12.3128", "5"))
+func TestDivf(t *testing.T) {
+	result := divf("6.12487", 2.347511)
+	if result != 2.6090910756115733 {
+		t.Errorf("result: %v", result)
+	}
+}
+
+func TestMulf(t *testing.T) {
+	result := mulf("6.12487", 2.347511)
+	if result != 14.37819969857 {
+		t.Errorf("result: %v", result)
+	}
+}
+
+func TestRandInt(t *testing.T) {
+	result := randInt(50, 55)
+	if result < 50 || result > 55 {
+		t.Errorf("result: %v", result)
 	}
 }
 
 func TestRound(t *testing.T) {
 	if round("6.32487", 2) != 6.32 {
-		t.Errorf("round unexpected result: %v", round("6.32487", 2))
+		t.Errorf("result: %v", round("6.32487", 2))
+	}
+}
+
+func TestMax(t *testing.T) {
+	if max("6", 4, "12", "5") != 12 {
+		t.Errorf("result: %v", max("6", 4, "12", "5"))
+	}
+}
+
+func TestMin(t *testing.T) {
+	if min("6", 4, "12", "5") != 4 {
+		t.Errorf("result: %v", min("6", 4, "12", "5"))
+	}
+}
+
+func TestMaxf(t *testing.T) {
+	if maxf("6.32", 4.15, "12.3128", "5") != 12.3128 {
+		t.Errorf("result: %v", maxf("6.32", 4.15, "12.3128", "5"))
+	}
+}
+
+func TestMinf(t *testing.T) {
+	if minf("6.32", 4.15, "12.3128", "5") != 4.15 {
+		t.Errorf("result: %v", minf("6.32", 4.15, "12.3128", "5"))
+	}
+}
+
+func TestDateFormat(t *testing.T) {
+	if dateFormat("15.03.2021", "02.01.2006", "01022006") != "03152021" {
+		t.Errorf("result: %s", dateFormat("15.03.2021", "02.01.2006", "01022006"))
+	}
+}
+
+func TestNow(t *testing.T) {
+	if now("2006-01-02 15:04") != time.Now().Format("2006-01-02 15:04") {
+		t.Errorf("result: %s", now("2006-01-02 15:04"))
 	}
 }
 
 func TestBase64encode(t *testing.T) {
 	if base64encode("Hello World!") != "SGVsbG8gV29ybGQh" {
-		t.Errorf("base64encode unexpected result: %v", base64encode("Hello World!"))
+		t.Errorf("result: %v", base64encode("Hello World!"))
 	}
 }
 
 func TestBase64decode(t *testing.T) {
 	if base64decode("SGVsbG8gV29ybGQh") != "Hello World!" {
-		t.Errorf("base64decode unexpected result: %v", base64decode("SGVsbG8gV29ybGQh"))
+		t.Errorf("result: %v", base64decode("SGVsbG8gV29ybGQh"))
 	}
 }
 
 func TestBase32encode(t *testing.T) {
 	if base32encode("Hello World!") != "JBSWY3DPEBLW64TMMQQQ====" {
-		t.Errorf("base32encode unexpected result: %v", base32encode("Hello World!"))
+		t.Errorf("result: %v", base32encode("Hello World!"))
 	}
 }
 
 func TestBase32decode(t *testing.T) {
 	if base32decode("JBSWY3DPEBLW64TMMQQQ====") != "Hello World!" {
-		t.Errorf("base32decode unexpected result: %v", base32decode("JBSWY3DPEBLW64TMMQQQ===="))
-	}
-}
-
-func TestToFloat64(t *testing.T) {
-	if toFloat64("3.14159265") != 3.14159265 {
-		t.Errorf("toFloat64 unexpected result: %v", toFloat64("3.14159265"))
-	}
-}
-
-func TestToInt(t *testing.T) {
-	if toInt("42") != 42 {
-		t.Errorf("toInt unexpected result: %d", toInt("42"))
-	}
-}
-
-func TestToInt64(t *testing.T) {
-	if toInt64("42") != 42 {
-		t.Errorf("toInt unexpected result: %d", toInt64("42"))
+		t.Errorf("result: %v", base32decode("JBSWY3DPEBLW64TMMQQQ===="))
 	}
 }
 
 func TestRegexMatch(t *testing.T) {
 	if !regexMatch("a.b", "aaxbb") {
-		t.Errorf("regexMatch unexpected result: %v", regexMatch(`^a.b$`, "aaxbb"))
+		t.Errorf("result: %v", regexMatch(`^a.b$`, "aaxbb"))
 	}
 }
+
+func TestUUID(t *testing.T) {
+	testUUID := newUUID()
+	r := regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")
+	if !r.MatchString(testUUID) {
+		t.Errorf("result: %v", testUUID)
+	}
+}
+
+func TestUpper(t *testing.T) {
+	if upper("Hello") != "HELLO" {
+		t.Errorf("result: %v", upper("Hello"))
+	}
+}
+
+func TestLower(t *testing.T) {
+	if lower("World") != "world" {
+		t.Errorf("result: %v", lower("World"))
+	}
+}
+
+func TestTrim(t *testing.T) {
+	result := trim("\r\nHello World\r\n")
+	if result != "Hello World" {
+		t.Errorf("result: %v", result)
+	}
+}
+
+func TestTrimAll(t *testing.T) {
+	result := trimAll("!Hello World!", "!")
+	if result != "Hello World" {
+		t.Errorf("result: %v", result)
+	}
+}
+
+func TestPrefix(t *testing.T) {
+	result := trimPrefix("!Hello World!", "!")
+	if result != "Hello World!" {
+		t.Errorf("result: %v", result)
+	}
+}
+
+func TestSuffix(t *testing.T) {
+	result := trimSuffix("!Hello World!", "!")
+	if result != "!Hello World" {
+		t.Errorf("result: %v", result)
+	}
+}
+
+func TestAtoi(t *testing.T) {
+	result := atoi("42")
+	if result != 42 {
+		t.Errorf("result: %v", result)
+	}
+}
+
+func TestToInt(t *testing.T) {
+	if toInt("42") != 42 {
+		t.Errorf("result: %d", toInt("42"))
+	}
+}
+
+func TestToInt64(t *testing.T) {
+	if toInt64("42") != 42 {
+		t.Errorf("result: %d", toInt64("42"))
+	}
+}
+
+func TestToFloat64(t *testing.T) {
+	if toFloat64("3.14159265") != 3.14159265 {
+		t.Errorf("result: %v", toFloat64("3.14159265"))
+	}
+}
+
+func TestLuaF(t *testing.T) {
+	if luaF("sum", "5", "5") != "10" {
+		t.Errorf("result: %s", luaF("sum", "5", "5"))
+	}
+}
+
+// TODO toXXX tests
+/*
+func TestToJSON(t *testing.T) {
+}
+*/
 
 func TestExecDecimalOp(t *testing.T) {
 	testMulf := func(a interface{}, v ...interface{}) float64 {
 		return execDecimalOp(a, v, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Mul(d2) })
 	}
 	if testMulf(6.2154, "4.35") != 27.03699 {
-		t.Errorf("testMulf unexpected result: %v", testMulf(6.2154, "4.35"))
+		t.Errorf("result: %v", testMulf(6.2154, "4.35"))
 	}
 	testDivf := func(a interface{}, v ...interface{}) float64 {
 		return execDecimalOp(a, v, func(d1, d2 decimal.Decimal) decimal.Decimal { return d1.Div(d2) })
 	}
 	if testDivf(6.2154, "4.35") != 1.4288275862068966 {
-		t.Errorf("testDivf unexpected result: %v", testDivf(6.2154, "4.35"))
+		t.Errorf("result: %v", testDivf(6.2154, "4.35"))
 	}
 }
